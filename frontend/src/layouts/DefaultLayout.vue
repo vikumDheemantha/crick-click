34e600baf174c01f84901bd155c6efef7eef6711
Vikum Dheemantha <vikum.dheemantha@gmail.com>
2021-09-12 20:44:29 +0530

    Setup layout system

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <app-bar />
+    <slot/>
+
+  </div>
+</template>
+
+<script>
+import AppBar from "../components/layout/AppBar.vue";
+export default {
+    components: {
+        AppBar
+    },
+}
+</script>
+
+<style>
+
+</style>
