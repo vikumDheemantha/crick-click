4c4f4be58816299579a99079830f2ef8e396c370
Vikum Dheemantha <vikum.dheemantha@gmail.com>
2021-09-19 22:05:50 +0530

    home page leates changes

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <v-content class="pt-0">
     <top-banner />
+    <breef-intro />
   </v-content>
 </template>
 
 <script>
+import BreefIntro from "../components/home/BreefIntro.vue";
 import TopBanner from "../components/home/TopBanner.vue";
 export default {
   name: "Home",
-
-  components: { TopBanner },
+  data() {
+    return {
+      image: null,
+    };
+  },
+  components: { TopBanner, BreefIntro },
 };
 </script>
