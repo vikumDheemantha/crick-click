87fad931c824756973f4801bcc1e66f5b406264c
Shafraz Buhary <developer@shafraz.net>
2021-10-18 15:15:54 +0530

    funtions for teams

--- frontend/src/firebase/players.firebase.js
+++ frontend/src/firebase/players.firebase.js
@@ -1,5 +1,6 @@
-import { collection, getDocs, getDoc, query, where } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 import { db } from "./main";
+import { passSocialMedioa } from "./socialmedia.firebase.js";
 
 export const getAllPlayers = async () => {
   let players = [];
@@ -15,5 +16,18 @@ export const getAllPlayers = async () => {
     });
   } catch (error) {
     console.error("Error fetching data from the firebase: ", error);
+  }
+  return players;
+};
+
+export const getPlayerById = async (id) => {
+  const docRef = doc(db, "players", id);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists) {
+    let socicalMedia = passSocialMedioa(docSnap.socicalMedia);
+    return { id: docSnap.id, socicalMedia: socicalMedia, ...docSnap.data() };
+  } else {
+    return {};
   }
 };
