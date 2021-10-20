f9eb2c42ac907973bf275bf47a7f6cd54d0e3cfb
Vikum Dheemantha <vikum.dheemantha@gmail.com>
2021-10-20 20:15:48 +0530

    team list

---
+++ frontend/src/components/teamList/TeamTable.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-card>
+    <v-data-table
+      :headers="headers"
+      :items="players"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Type", value: "type" },
+        { text: "City", value: "city" },
+        { text: "Region", value: "region" },
+        { text: "Email", value: "email" },
+        { text: "Phone No.", value: "phone" },
+      ],
+      players: [
+        {
+          name: "Frozen Yogurt",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Ice cream sandwich",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Eclair",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Cupcake",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Gingerbread",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Jelly bean",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Lollipop",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Honeycomb",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "Donut",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+        {
+          name: "KitKat",
+          type: "club",
+          city: "Kandy",
+          region: "Kandy",
+          email: "info@abc.com",
+          phone: "94523342203",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/views/teamList.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <h3 class="primary--text my-5">All Teams</h3>
+    <search-filter />
+    <v-row>
+      <v-col cols="12" md="8">
+        <team-table />
+      </v-col>
+      <v-col cols="12" md="4">
+        <team-profile-card />
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import SearchFilter from "../components/teamList/SearchFilter.vue";
+import TeamProfileCard from "../components/teamList/TeamProfileCard.vue";
+import TeamTable from "../components/teamList/TeamTable.vue";
+export default {
+  components: {
+    SearchFilter,
+    TeamTable,
+    TeamProfileCard,
+  },
+};
+</script>
+
+<style></style>
